6db8df3c60b5677f92da877185fe9458f9472216
Bouke Haarsma <bouke@haarsma.eu>
2017-09-16 11:33:29 +0200

    Upgrade to BigInt 3 (Swift 4)

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "SRP",
     dependencies: [
         .package(url: "https://github.com/IBM-Swift/BlueCryptor.git", from: "0.8.16"),
-        .package(url: "https://github.com/lorentey/BigInt.git", .branch("swift4")),
+        .package(url: "https://github.com/lorentey/BigInt.git", from: "3.0.0"),
     ],
     targets: [
         .target(name: "SRP", dependencies: ["Cryptor", "BigInt"], path: "Sources"),
