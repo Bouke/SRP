b166838d4cf9218933c03151d62e50772460f95e
Bouke Haarsma <bouke@haarsma.eu>
2017-09-18 20:39:25 +0200

    Add library product definition

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "SRP",
+    products: [
+        .library(name: "SRP", targets: ["SRP"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/IBM-Swift/BlueCryptor.git", from: "0.8.16"),
         .package(url: "https://github.com/lorentey/BigInt.git", from: "3.0.0"),
