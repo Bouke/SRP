75e07b3536a3c62504450840152461628e789c35
Bouke Haarsma <bouke@haarsma.eu>
2018-05-26 09:12:20 +0200

    Dependency moved to new URL

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/IBM-Swift/BlueCryptor.git", from: "0.8.16"),
-        .package(url: "https://github.com/lorentey/BigInt.git", from: "3.0.0"),
+        .package(url: "https://github.com/attaswift/BigInt.git", from: "3.0.0"),
     ],
     targets: [
         .target(name: "SRP", dependencies: ["Cryptor", "BigInt"], path: "Sources"),
